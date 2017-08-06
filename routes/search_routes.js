27ba42f6e46c8b5989644ea3848aacdbe035b567
Amidou Kante <kamdiou@gmail.com>
2017-08-05 22:55:25 -0400

    mobile and desktop css finished

--- routes/search_routes.js
+++ routes/search_routes.js
@@ -4,8 +4,6 @@ const searchRouter = express.Router();
 const searchController = require('../controllers/search_controller');
 const clothesHelper = require('../services/api/clothes_helper');
 
-// searchRouter.get('/', searchController.index);
-
 searchRouter.get('/', clothesHelper.productSearchAPI, searchController.sendSearch)
 
 module.exports = searchRouter;
