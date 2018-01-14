d84dbb2fb39d15f5c2f84831bfadbce65cc22852
Amidou Kante <kamdiou@gmail.com>
2018-01-14 18:27:57 -0500

    updated config file

--- db/config.js
+++ db/config.js
@@ -7,6 +7,8 @@ if (process.env.NODE_ENV === 'development' || !process.env.NODE_ENV) {
     database: 'armoire',
     port: 5432,
     host: 'localhost',
+    user: process.env.database_user,
+    password: process.env.database_user_password
   });
 } else if (process.env.NODE_ENV === 'production') {
   db = pgp(process.env.DATABASE_URL);
